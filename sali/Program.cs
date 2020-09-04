2b0d29a878aa0574f1bd09fd46efc4010b9049a8
Mika Vainio <koti.mika.vainio@gmail.com>
2020-09-04 16:23:12 +0300

    Korjattu VS:n kaatumisvahinkoja

--- sali/Program.cs
+++ sali/Program.cs
@@ -2,40 +2,23 @@
 
 namespace sali
 {
-    public class Asiakas
-{
-	// Kentät perustietoja varten
-	protected string etunimi;
-	protected string sukunimi;
-	protected float pituus;
-	protected float paino;
-
-	// Muodostin kaikilla argumenteilla
-	public Asiakas(string etunimi, string sukunimi, float pituus, float paino)
-	{
-		this.etunimi = etunimi;
-		this.sukunimi = sukunimi;
-		this.pituus = pituus;
-		this.paino = paino;
-	}
-
-	// Metodit
-
-	// Painoindeksin laskenta
-	public void BMI()
-    {
-		float bmi = this.paino / (this.pituus * this.pituus);
-		Console.WriteLine(bmi);
-    }
-}
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string kenganNumero = "42";
-            Console.WriteLine(kenganNumero);
+            Console.WriteLine("Testataan asikasluokkaa");
+			Console.WriteLine("Anna etunimi");
+			string etunimi = Console.ReadLine();
+			Console.WriteLine("Anna sukunimi");
+			string sukunimi = Console.ReadLine();
+			Console.WriteLine("Syötä pituus");
+			float pituus = float.Parse(Console.ReadLine());
+			Console.WriteLine("Syötä paino");
+			float paino = float.Parse(Console.ReadLine());
+			
+			Asiakas asiakas = new Asiakas(etunimi, sukunimi, pituus, paino);
+			asiakas.BMI();
+            
         }
     }
 }
