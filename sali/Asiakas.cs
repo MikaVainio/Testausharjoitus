a88c824de7f8055ae360d18494e16bc1aa5247e2
Mika Vainio <koti.mika.vainio@gmail.com>
2020-09-04 16:35:00 +0300

    Korjattu kaatumisen aiheuttamia vahinkoja

---
+++ sali/Asiakas.cs
@@ -0,0 +1,32 @@
+﻿// Määritellään ulkoiset nimiavaruudet
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Määritellään luokan nimiavaruus, sama kuin pääohjelmalla
+namespace sali
+{
+    // Määritellään sovelluksen sisäinen luokka Asiakas
+    internal class Asiakas
+    {
+        private string etunimi;
+        private string sukunimi;
+        private float pituus;
+        private float paino;
+
+        // Muodostin kaikilla parametreilla
+        public Asiakas(string etunimi, string sukunimi, float pituus, float paino)
+        {
+            this.etunimi = etunimi;
+            this.sukunimi = sukunimi;
+            this.pituus = pituus;
+            this.paino = paino;
+        }
+
+        // Metodi, joka tulostaa ruudulle painoindeksin
+        public void BMI()
+        {
+            Console.WriteLine("Painoindeksisi on " + this.paino / (this.pituus * this.pituus));
+        }
+    }
+}
--- sali/Program.cs
+++ sali/Program.cs
@@ -1,22 +1,28 @@
-﻿using System;
+﻿// Määritellään ulkoiset nimiavaruudet
+using System;
 
+// Määritellään pääohjelman nimiavaruus
 namespace sali
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testataan asikasluokkaa");
+            // Kysytään asiakkaan tiedot konsolilta
+			Console.WriteLine("Testataan asikasluokkaa");
 			Console.WriteLine("Anna etunimi");
 			string etunimi = Console.ReadLine();
 			Console.WriteLine("Anna sukunimi");
 			string sukunimi = Console.ReadLine();
-			Console.WriteLine("Syötä pituus");
+			Console.WriteLine("Syötä pituus metreinä");
 			float pituus = float.Parse(Console.ReadLine());
-			Console.WriteLine("Syötä paino");
+			Console.WriteLine("Syötä paino kiloina");
 			float paino = float.Parse(Console.ReadLine());
 			
+			// Luodaan Asiakas-olio luokasta, joka on määritelty tiedostossa Asiakas.cs
 			Asiakas asiakas = new Asiakas(etunimi, sukunimi, pituus, paino);
+			
+			// Kutsutaan metodia, joka tulostaa painoindeksin
 			asiakas.BMI();
             
         }
