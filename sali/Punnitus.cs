de26317ab03697ac2272106f7bb6ada9d490c32f
Mika Vainio <koti.mika.vainio@gmail.com>
2020-09-09 19:59:01 +0300

    Kommentoitu lisää

--- sali/Punnitus.cs
+++ sali/Punnitus.cs
@@ -7,8 +7,10 @@ namespace Jokumuu
 {
     class Punnitus
     {
+        // Määritellään paino-kenttä kaikkialla käytettäväksi
         public float paino;
 
+        // Muodostin kaikilla parametreilla
         public Punnitus(float paino)
         {
             this.paino = paino;
